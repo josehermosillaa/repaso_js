cbb768620e737f2b784d1b3c2494f1451ee58205
josehermosillaa <jose.hermosillaa@usach.cl>
2021-09-27 08:24:39 -0300

    quedamos al final del ultimo capitulo

---
+++ 01_Fundamentos/assets/js/app.js
@@ -0,0 +1,48 @@
+// //life motto
+// if (sad() === true ){
+//     sad().stop;
+//     beAwesome();
+// }
+// simepre las funciones tienen un return
+// console.log("Hola mundo ");
+// con document referenciamos el texto
+// document.write("Hola Mundo!"); // esto reemplaza el HTMML del documento
+
+//variables
+console.log(miNuevonombre + ' hola estupido');
+// let a = 10;
+// alert('Hola desde app.js')
+let a = 10, 
+    b = 20, 
+    c = 'Hola ', 
+    d = 'Spiderman', 
+    x = a + b ;
+// var b = 10;
+// const c = 10;
+// let d = 10 ;
+
+// let x = a+b;
+
+// console.log( a );
+
+// console.warn( b );
+// console.error( c );
+//si quiero ver la variable console.log( 'a', a ); mucho texto
+//warn y err
+// console.log('%c Mis varibales', 'color:blue; font-weight:bold')
+// console.log( { a } );
+// console.log( { b }  );
+// console.log( { c } );
+
+// console.table({a, b, c, d});
+
+const saludo = c + d; //SALUDO eso en todos los lugares, se dice que cuando tiene el mismo valor en todos los entornos
+
+// console.log(saludo)
+
+c = 'Hola de nuevo'; //en el breakpoint se observa como cambia 
+
+// let outerWidth = 100000000 ;//lo coloca en el objeto WINDOW, no es buena practica
+// let outerHeight = 600; //el elt y el const no sobrescriben las variables del objeto Window
+
+let miNuevonombre = 'Fernando Herrera'; //con let  podemos tener un codigo mas fino
